3297cf7cfa0b5cf550500bd4ab3d761b48add779
jashanshah33 <jashanshah33@gmail.com>
2022-10-17 20:33:16 -0400

    Init commit
    mend
    
    Signed-off-by: jashanshah33 <jashanshah33@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCInYR39ZDnASmmngwI9mIdF2q5kgZO60Y",
-    authDomain: "cart-21f82.firebaseapp.com",
-    projectId: "cart-21f82",
-    storageBucket: "cart-21f82.appspot.com",
-    messagingSenderId: "420116899040",
-    appId: "1:420116899040:web:f13cf28d2127aaf17e5c85"
-  };
-  
-  // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCInYR39ZDnASmmngwI9mIdF2q5kgZO60Y",
+  authDomain: "cart-21f82.firebaseapp.com",
+  projectId: "cart-21f82",
+  storageBucket: "cart-21f82.appspot.com",
+  messagingSenderId: "420116899040",
+  appId: "1:420116899040:web:f13cf28d2127aaf17e5c85",
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
